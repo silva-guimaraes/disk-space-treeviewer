4af8e813fb73e8693e444f3c0c49f3c5f4d9d2a7
silva guimaraes <silva__guimaraes@hotmail.com>
2024-03-29 12:14:19 -0300

    documentação

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+
 import (
 	_ "embed"
 	"errors"
@@ -8,12 +9,10 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
-	// "strings"
 )
 
 type dir struct {
     abs, Relative string
-    // stat                fs.FileInfo
     Children            []*dir
     SizeFiles, SizeDirs int64
 }
@@ -51,21 +50,64 @@ func main() {
 
     counter := 0
     totalSize := int64(0)
+
+    // essa gambiarra toda é pra transformar uma lista de diretórios em uma árvore de diretórios já que
+    // fs.WalkDir retorna um diretório de cada vez de maneira desestruturada.
+
+    // realidade:
+    // root/
+    // ├─ bar/
+    // │  ├─ foo/
+    // │  │  ├─ barfoo/
+    // │  │  │  ├─ foobar/
+    // │  ├─ barfoo/
+    // │  ├─ foofoo/
+    // ├─ foo/
+    // ├─ foobar/
+    // │  ├─ foo/
+
+    // fs.WalkDir:
+    // root
+    // root/bar
+    // root/bar/foo
+    // root/bar/foo/barfoo
+    // root/bar/foo/barfoo/foobar
+    // root/bar/barfoo
+    // root/bar/foofoo
+    // root/foo
+    // root/foobar
+    // root/foobar/foo
+
+    // pra nossa sorte, fs.WalkDir retorna os diretórios em ordem alfabética, então, cada novo diretório introduzido
+    // sempre será filho do diretório atual.
+
+
+    // stack de diretórios. o ultimo elemento dessa lista é o diretório atual onde o tamanho dos arquivos e outros
+    // diretórios subsequentes são salvos
     pwd := []*dir{}
 
+    // faz com que o ultimo diretório passe a ser filho do diretório anterior
+    // isso monta a nossa arvore ao final do fs.WalkDir
     popPwd := func() {
         pop := pwd[len(pwd)-1]
         pwd = pwd[:len(pwd)-1]
 
         current := pwd[len(pwd)-1]
         current.Children = append(current.Children, pop)
+
+        // todo o tamanho que os filhos desse diretório possuem
         current.SizeDirs += pop.SizeDirs + pop.SizeFiles
     }
 
+    // a cada novo diretório encontrado, pushar para a stack e tratar novo diretório como diretório atual.
+    // apenas diretórios são salvos. cada arquivo encontrado tem o seu tamanho salvo no seu diretório parente.
+    // se novo diretório não ter o mesmo parente que o diretório atual, isso significa que esses dois diretórios
+    // são irmãos possivelmente, popPwd nesse caso.
     fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, fserr error) error {
 
         abs := filepath.Join(rootPath, path)
 
+        // essa pasta não possue nada de importante e só me causa dor de cabeça.
         if abs == "/proc" {
             fmt.Println(abs)
             fmt.Println("problematic folder, skipping:", "/proc")
@@ -112,19 +154,21 @@ func main() {
         return nil
     })
 
-
+    // capaz que tenham sobrado diretórios no pwd. isso transforma toda a arvore em um root apenas.
     for len(pwd) > 1 {
         popPwd()
     }
 
-    formatted := float64(totalSize)
-    magnitude := []rune{'B', 'K', 'M', 'G'}
-    magnitudeIndex := 0
+    // fim da gambiarra
 
-    for formatted > 1000 && magnitudeIndex < len(magnitude)-1 {
-        formatted /= 1000
-        magnitudeIndex++
-    }
+    // formatar tamanho
+    // formatted := float64(totalSize)
+    // magnitude := []rune{'B', 'K', 'M', 'G'}
+    // magnitudeIndex := 0
+    // for formatted > 1000 && magnitudeIndex < len(magnitude)-1 {
+    //     formatted /= 1000
+    //     magnitudeIndex++
+    // }
 
     t, err := template.New("foo").Parse(templ) 
     if err != nil {
